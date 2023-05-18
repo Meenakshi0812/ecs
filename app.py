51fa7e3eacc71b4c0475fee3002b7391a41b4c03
meenakshi0812 <gowrameenakshi@gmail.com>
2023-05-18 18:14:12 +0530

    updated

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 # app.py
 
-import datetime
+import requests
+import random
 
-def get_current_time():
-    current_time = datetime.datetime.now()
-    return str(current_time)
+def save_random_image():
+    url = "https://source.unsplash.com/random"
+    response = requests.get(url)
+    if response.status_code == 200:
+        image_name = f"random_image_{random.randint(1, 100)}.jpg"
+        with open(image_name, 'wb') as image_file:
+            image_file.write(response.content)
+        return image_name
+    else:
+        return None
 
 if __name__ == '__main__':
-    print(f"The current time is: {get_current_time()}")
+    image_file = save_random_image()
+    if image_file:
+        print(f"Random image saved as: {image_file}")
+    else:
+        print("Failed to fetch random image.")
